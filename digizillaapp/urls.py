9e8c83c282565e539598e5518ebfb09e0c81b4e3
Hossam Khalil <hossamkhalil001@gmail.com>
2021-05-18 19:10:34 +0200

    feature(auth): created login page

--- digizillaapp/urls.py
+++ digizillaapp/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from .settings import STATIC_ROOT, STATIC_URL
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-]
+]+ static(STATIC_URL, document_root=STATIC_ROOT)
