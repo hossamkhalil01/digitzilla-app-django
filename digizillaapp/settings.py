f97edea8495c7269af55bb44f7583a71de96e75e
Hossam Khalil <hossamkhalil001@gmail.com>
2021-05-19 01:02:16 +0200

    feature(home-page): created homepage template and layout

--- digizillaapp/settings.py
+++ digizillaapp/settings.py
@@ -153,8 +153,12 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
-# coming from the secrets file
+# Coming from the secrets file
 EMAIL_HOST_USER = EMAIL_SECRETS['email']
 EMAIL_HOST_PASSWORD = EMAIL_SECRETS['password']
 DEFAULT_FROM_EMAIL = EMAIL_SECRETS['email']
+
+
+# Redirects configurations
+LOGIN_REDIRECT_URL = 'user_home'
+LOGOUT_REDIRECT_URL = 'login'
