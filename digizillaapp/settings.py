f6d54970be05bfdcc86a97ad60703817bb6dee8e
Hossam Khalil <hossamkhalil001@gmail.com>
2021-05-18 14:29:46 +0200

    feature(user): created table migrations

--- digizillaapp/settings.py
+++ digizillaapp/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# define the custom user model
+AUTH_USER_MODEL = 'user.User'
