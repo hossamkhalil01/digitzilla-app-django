a52c665fd744044567819be11a228918034a377d
Hossam Khalil <hossamkhalil001@gmail.com>
2021-05-18 14:46:33 +0200

    feature(user-app): created user profile views and url

--- user/views.py
+++ user/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import User
 
-# Create your views here.
+
+@login_required
+def profile(request):
+    # pass the current user
+    return render(request, 'profile.html', {'current_user': request.user})
